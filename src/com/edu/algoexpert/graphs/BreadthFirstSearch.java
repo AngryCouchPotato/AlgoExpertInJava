b711143bd1f9c6226f454b301c45455c2025f09e
AngryCouchPotato <alexeykolcov@gmail.com>
2020-11-01 19:40:35 +0100

    solve 1 task:   Breadth-first search

---
+++ src/com/edu/algoexpert/graphs/BreadthFirstSearch.java
@@ -0,0 +1,40 @@
+package com.edu.algoexpert.graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BreadthFirstSearch {
+
+    static class Node {
+
+        String name;
+        List<Node> children = new ArrayList<>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> breadthFirstSearch(List<String> array) {
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(this);
+            while (!queue.isEmpty()) {
+                Node node = queue.poll();
+                array.add(node.name);
+                for (Node child : node.children) {
+                    if (child != null) {
+                        queue.add(child);
+                    }
+                }
+            }
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+}
